272f6a15ae0e8060f343fde372b37fef833297ce
whikih34 <gitacc34@gmail.com>
2025-02-28 18:09:03 +0900

    ✨ Add PhotoNavigator in AppNavigator
    Add PhotoNavigator in AppNavigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -8,6 +8,9 @@ import BottomTabNavigator from './BottomTabNavigator';
 // ✅ Shorts 관련 네비게이션
 import ShortsNavigator from './ShortsNavigator';
 
+// ✅ Photo 관련 네비게이션 추가
+import PhotoNavigator from './PhotoNavigator'; // ✅ 추가
+
 // ✅ Stack Navigator 생성
 const Stack = createStackNavigator();
 
@@ -17,9 +20,10 @@ const AppNavigator = () => {
       <Stack.Navigator screenOptions={{headerShown: false}}>
         {/* ✅ 메인 하단 탭 네비게이션 */}
         <Stack.Screen name="Main" component={BottomTabNavigator} />
-
-        {/* ✅ Shorts 관련 네비게이션 추가 */}
+        {/* ✅ Shorts 관련 네비게이션 */}
         <Stack.Screen name="ShortsStack" component={ShortsNavigator} />
+        {/* ✅ Photo 관련 네비게이션 추가 */}
+        <Stack.Screen name="Photo" component={PhotoNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
